8743ee528e0abaef114222b19fee5900e50a6dd7
sonu28gpt <98702109+sonu28gpt@users.noreply.github.com>
2025-01-16 02:14:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
+
 //your code here
+let draggedEL=null;
+let droppedEL=null;
+
+function dragStart(e) {
+	draggedEL=e.target;
+}
+function dragOver(e){
+	e.preventDefault();
+}
+function drop(e) {
+	droppedEL=e.target;
+  let dragStyle=window.getComputedStyle(draggedEL);
+  let dropStyle=window.getComputedStyle(droppedEL);
+  let temp=dragStyle.backgroundImage;
+  draggedEL.style.backgroundImage=dropStyle.backgroundImage;
+  droppedEL.style.backgroundImage=temp;
+    draggedEL=null;
+    droppedEL=null;
+}
+	
