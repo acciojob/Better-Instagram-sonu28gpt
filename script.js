297c24091e34dac8a2730ac4ec8f020875ce5daa
sonu28gpt <98702109+sonu28gpt@users.noreply.github.com>
2025-01-16 02:42:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -13,9 +13,12 @@ function drop(e) {
 	droppedEL=e.target;
   let dragStyle=window.getComputedStyle(draggedEL);
   let dropStyle=window.getComputedStyle(droppedEL);
-  let temp=dragStyle.backgroundImage;
-  draggedEL.style.backgroundImage=dropStyle.backgroundImage;
-  droppedEL.style.backgroundImage=temp;
+   let tempid=draggedEL.getAttribute('id');
+  draggedEL.setAttribute('id',droppedEL.getAttribute('id'));
+  droppedEL.setAttribute('id',tempid);
+  let tempContent=draggedEL.textContent;
+  draggedEL.textContent=droppedEL.textContent;
+  droppedEL.textContent=tempContent;
     draggedEL=null;
     droppedEL=null;
 }
